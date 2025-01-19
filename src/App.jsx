bdfe1c8388ed0861c72c4b2dbc26b624f765e1a3
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2025-01-20 02:45:46 +0530

    Project scafold

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Nav from './components/Nav'
+
+const App = () => (
+  <main>
+    <Nav/>
+    <section>
+      <h1>sidebar</h1>
+    </section>
+    <section>
+      <h1>content</h1>
+    </section>
+    <section>
+      <h1>footer</h1>
+    </section>
+  </main>
+)
+
+export default App
