8162fb7b3763b63264307b6101d7a5f016721ea3
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2025-01-20 04:56:34 +0530

    aas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,12 +10,33 @@ const Nav = () => {
   }
 
   return (
-    <header className="bg-gray-800 p-7">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+    <header className="bg-black py-4">
+      <div className="container mx-5 flex flex-wrap items-center justify-between">
         {/* Company Logo */}
         <div className="flex items-center">
-          <img src={logo} alt="Kochas Power Pvt Ltd Logo" className="h-10 w-10 mr-2" />
-          <h1 className="text-white text-2xl font-bold">Kochas Power Pvt Ltd</h1>
+          <img 
+            src={logo} 
+            alt="Kochas Power Pvt Ltd Logo" 
+            className="h-[90px] w-[90px] mr-2 rounded-full transition-transform duration-300 hover:scale-110 hover:shadow-xl hover:shadow-white" 
+          />
+          <h1 className="text-white text-2xl font-bold rounded-full px-2 transition-transform duration-300 hover:scale-110 hover:shadow-lg hover:shadow-white">
+            Kochas Power Pvt Ltd
+          </h1>
+        </div>
+        
+        {/* Navigation Links */}
+        <nav className={` w-full md:w-auto ${isOpen ? 'block' : 'hidden'} md:block`}>
+          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mt-2 md:mt-0 justify-center w-full md:w-auto">
+            <li><a href="#home" className="px-5 text-lg text-white transition-transform duration-300 hover:scale-110 hover:text-gray-400 hover:shadow-lg">Home</a></li>
+            <li><a href="#about" className="px-5 text-lg text-white transition-transform duration-300 hover:scale-110 hover:text-gray-400 hover:shadow-lg">About</a></li>
+            <li><a href="#contact" className="px-5 text-lg text-white transition-transform duration-300 hover:scale-110 hover:text-gray-400 hover:shadow-lg">Contact</a></li>
+          </ul>
+        </nav>
+        
+        {/* Profile Section */}
+        <div className="flex items-center align-middle">
+          <div className="text-white mr-4">Profile</div>
+          <div className="h-10 w-10 rounded-full bg-gray-500"></div>
         </div>
         
         {/* Menu Toggle Button for Mobile */}
@@ -24,15 +45,6 @@ const Nav = () => {
             {isOpen ? <FaTimes /> : <FaBars />}
           </button>
         </div>
-        
-        {/* Navigation Links */}
-        <nav className={`w-full md:w-auto ${isOpen ? 'block' : 'hidden'} md:block`}>
-          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mt-2 md:mt-0">
-            <li><a href="#home" className="text-white hover:text-gray-400">Home</a></li>
-            <li><a href="#about" className="text-white hover:text-gray-400">About</a></li>
-            <li><a href="#contact" className="text-white hover:text-gray-400">Contact</a></li>
-          </ul>
-        </nav>
       </div>
     </header>
   )
