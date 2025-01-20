56976d15d46e6181f7daa0fdd6ac2644616bb72e
Sandesh007711 <sandesh12345678q@gmail.com>
2025-01-20 06:14:22 +0530

    sidebar beautiful

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { FaUser, FaCar, FaDollarSign, FaKey, FaChartBar, FaBars } from 'react-icons/fa'
+import { FaUser, FaCar, FaDollarSign, FaKey, FaChartBar, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import './Sidebar.css'
 
 const Sidebar = () => {
@@ -10,11 +10,13 @@ const Sidebar = () => {
   }
 
   return (
-    <div className='fixed'>
-      <button className="toggle-button" onClick={toggleSidebar}>
-        <FaBars />
-      </button>
-      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div>
+      <div className="toggle-button-container" onMouseEnter={toggleSidebar}>
+        <button className="toggle-button" onClick={toggleSidebar}>
+          {isOpen ? <FaArrowLeft /> : <FaArrowRight />}
+        </button>
+      </div>
+      <div className={`sidebar ${isOpen ? 'open' : ''}`} onMouseLeave={toggleSidebar}>
         <ul>
           <li><FaUser /><span>Create User</span></li>
           <li><FaCar /><span>Vehicle</span></li>
