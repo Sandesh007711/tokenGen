2920670932f5e1cf77c0708e57a07c8d3ba2d817
Erie Lama <dheeraj.singh1994@gmail.com>
2025-01-21 03:33:17 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css' // Import the CSS file for styling
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>COPYRIGHT &copy; KOCHAS POWER PVT. LTD. ALL RIGHTS RESERVED</p>
+      <p>COPYRIGHT &copy; KOCHAS POWER PVT. LTD. ALL RIGHTS RESERVED - 2025</p>
     </footer>
   )
 }
