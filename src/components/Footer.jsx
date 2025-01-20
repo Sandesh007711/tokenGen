da89cd43275a8383c326292972dda06907783e8b
Sandesh007711 <sandesh12345678q@gmail.com>
2025-01-20 15:08:21 +0530

    fotter completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css' // Import the CSS file for styling
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; KOCHAS POWER PVT. LTD. ALL RIGHTS RESERVED</p>
+      <p>COPYRIGH &copy; KOCHAS POWER PVT. LTD. ALL RIGHTS RESERVED</p>
     </footer>
   )
 }
