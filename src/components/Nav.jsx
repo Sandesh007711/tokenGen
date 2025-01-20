08b15fd285feae80f3d3d433403547a09945077f
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2025-01-20 13:28:59 +0530

    Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,8 +10,8 @@ const Nav = () => {
   }
 
   return (
-    <header className="bg-black py-4 shadow-lg shadow-yellow-400">
-      <div className="flex justify-between items-center container mx-5"> 
+    <header className="bg-black py-4 ">
+      <div className="flex justify-between items-center mx-5"> 
         {/* Company Logo */}
         <div className="flex items-center">
           <img 
@@ -19,13 +19,13 @@ const Nav = () => {
             alt="Kochas Power Pvt Ltd Logo" 
             className="h-[110px] w-[110px] mr-2 rounded-full transition-transform duration-300 hover:scale-110 hover:shadow-xl hover:shadow-white" 
           />
-          <h1 className="text-white text-3xl font-bold rounded-full px-2 transition-transform duration-300 hover:scale-110 hover:shadow-lg hover:shadow-white">
+          <h1 className="text-white text-3xl font-bold rounded-xl px-2 transition-transform duration-300 hover:scale-110 hover:shadow-lg hover:shadow-white">
             Kochas Power Pvt. Ltd.
           </h1>
         </div>
         
         {/* Navigation Links */}
-        <nav className={` w-full md:w-auto ${isOpen ? 'block' : 'hidden'} md:block`}>
+        <nav className={` w-full md:w-auto ${isOpen ? 'block' : 'hidden'} md:block `}>
           <ul className="flex flex-col md:flex-row md:space-x-4 ">
             <li><a href="#home" className="text-2xl mx-4 hover:font-bold  text-white transition-transform duration-300 hover:scale-110 hover:text-yellow-400 hover:shadow-lg">Home</a></li>
             <li><a href="#about" className="text-2xl mx-4 hover:font-bold  text-white transition-transform duration-300 hover:scale-110 hover:text-yellow-400 hover:shadow-lg">About</a></li>
@@ -34,8 +34,9 @@ const Nav = () => {
         </nav>
         
         {/* Profile Section */}
-        <div >
-          <div className="text-2xl mx-4 hover:font-bold  text-white transition-transform duration-300 hover:scale-110 hover:text-yellow-400 hover:shadow-lg ">Profile</div>
+        <div className='flex flex-row px-[50px] ' >
+          <div className="flex-auto text-2xl mx-2 my-2 hover:font-bold  text-white transition-transform duration-300 hover:scale-110 hover:text-yellow-400 hover:shadow-lg ">Profile</div>
+          <div className="flex-auto  mx-4 h-[50px] w-[50px] rounded-full bg-slate-400 "></div>
         </div>
         
         {/* Menu Toggle Button for Mobile */}
