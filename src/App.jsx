55efcf726cbf2bd94c578a016efbfef8795411d7
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2025-01-20 15:10:04 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Nav from './components/Nav'
 import Sidebar from './components/Sidebar'
 import Content from './components/Content'
 import Footer from './components/Footer'
 
-const App = () => (
-  <main>
-    <Nav/>
-    {/*sandesh*/}
-    <section>
-      <Sidebar/>
-    </section>
-    <section>
-      <Content/>
-    </section>
-    <section>
-      <Footer/>
-    </section>
-  </main>
-)
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <main>
+      <Nav />
+      <section>
+        <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      </section>
+      <section className={`content ${isOpen ? 'content-shift' : ''}`}>
+        <Content isOpen={isOpen} />
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </main>
+  )
+}
 
 export default App
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import 'tailwindcss/tailwind.css'
 
-const Content = () => {
+const Content = ({ isOpen }) => {
   return (
-    <div>Contsdsdsdsent</div>
+    <div className={`p-4 transition-all duration-300 ${isOpen ? 'ml-64' : 'ml-0'}`}>
+    <table className="min-w-full bg-white border border-gray-200">
+      <thead>
+          <tr className="w-full bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+            <th className="py-3 px-6 text-left">Header 1</th>
+            <th className="py-3 px-6 text-left">Header 2</th>
+            <th className="py-3 px-6 text-left">Header 3</th>
+          </tr>
+        </thead>
+        <tbody className="text-gray-600 text-sm font-light">
+          <tr className="border-b border-gray-200 hover:bg-gray-100">
+            <td className="py-3 px-6 text-left">Data 1</td>
+            <td className="py-3 px-6 text-left">Data 2</td>
+            <td className="py-3 px-6 text-left">Data 3</td>
+          </tr>
+          <tr className="border-b border-gray-200 hover:bg-gray-100">
+            <td className="py-3 px-6 text-left">Data 4</td>
+            <td className="py-3 px-6 text-left">Data 5</td>
+            <td className="py-3 px-6 text-left">Data 6</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
