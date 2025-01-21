f334c46f51f654f6380203f596182a79352a3074
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2025-01-21 15:27:56 +0530

    Login Ini

--- src/routes/W_Routes.jsx
+++ src/routes/W_Routes.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import OtpVerification from '../pages/OtpVerification';
+import Login from '../pages/Login';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/otp-verification' element={<OtpVerification />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   );
 };
