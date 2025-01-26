79b596869be6507827b48fe65234dde58eb7f5b2
sandy <sandesh12345678q@gmail.com>
2025-01-26 18:16:23 +0530

    op home done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ export default {
     extend: {
       keyframes: {
         'slide-in-top': {
-          '0%': { transform: 'translateY(-1rem)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 },
-        }
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
       },
       animation: {
         'slide-in-top': 'slide-in-top 0.3s ease-out',
-      }
+      },
     },
   },
   plugins: [],
